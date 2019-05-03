27a38d07019321576c69d0a1e2707e48fa9fb7dd
Devon Brodka <devonbrodka@localhost.local>
2019-05-03 09:56:11 -0400

    initial pull

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'tachyons';
+import App from './containers/App.js';
+// import CardList from './CardList';
+
+ReactDOM.render(
+		<App />
+, document.getElementById('root'));
+
